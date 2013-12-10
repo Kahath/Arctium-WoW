de1f992acfbfe6cb8574eb58920f14f43274775d
Kahath <slayer_Lac@hotmail.com>
2013-12-10 01:43:09 +0100

    Fixed Convert group, dungeon difficulty change, raid difficulty change, promote assist, move raid member and group uninvite

--- WorldServer/Game/Packets/PacketHandler/GroupHandlers.cs
+++ WorldServer/Game/Packets/PacketHandler/GroupHandlers.cs
@@ -4,7 +4,6 @@ using Framework.Network.Packets;
 using Framework.ObjectDefines;
 using WorldServer.Network;
 using Framework.Logging;
-using Framework.Constants;
 using WorldServer.Game.WorldEntities;
 using System.Collections.Generic;
 
@@ -13,6 +12,115 @@ namespace WorldServer.Game.Packets.PacketHandler
     public class GroupHandler : Globals
     {
         static ulong PartyGUID = 1;
+
+        [Opcode(ClientMessage.GroupUninvite, "17538")]
+        public static void HandleGroupUninvite(
+            ref PacketReader packet, WorldClass session)
+        {
+            bool[] guidMask = new bool[8];
+            packet.Skip(1);
+
+            BitUnpack BitUnpack = new BitUnpack(packet);
+
+            guidMask[0] = BitUnpack.GetBit();
+
+            BitUnpack.GetBits<byte>(8);
+
+            for (int i = 1; i < 8; i++)
+                guidMask[i] = BitUnpack.GetBit();
+
+            ulong GUID = BitUnpack.GetPackedValue(guidMask,
+                new byte[] { 3, 4, 1, 7, 2, 0, 5, 6 },
+                new byte[] { 0, 4, 5, 7, 2, 3, 1, 6 });
+
+            session.Character.Group.Uninvite(GUID);
+        }
+
+        [Opcode(ClientMessage.GroupChangeRaidDifficulty, "17538")]
+        public static void HandleGroupChangeRaidDifficulty(
+            ref PacketReader packet, WorldClass session)
+        {
+            session.Character.Group.RaidDifficulty = (GroupDungeonDifficulty)packet.ReadUInt32();
+            session.Character.Group.Update();
+        }
+
+        [Opcode(ClientMessage.GroupMoveRaidMember, "17538")]
+        public static void HandleGroupMoveRaidMember(
+            ref PacketReader packet, WorldClass session)
+        {
+            packet.Skip(1);
+            var group = packet.ReadByte();
+
+            BitUnpack BitUnpack = new BitUnpack(packet);
+
+            ulong GUID = BitUnpack.GetPackedValue(
+                new byte[] { 3, 4, 2, 5, 1, 6, 7, 0 },
+                new byte[] { 6, 3, 7, 5, 1, 4, 2, 0 });
+
+            var pSession = WorldMgr.GetSession(GUID);
+
+            if (pSession == null)
+                return;
+
+            pSession.Character.Group.ChangeRaidGroup(pSession.Character.Guid, (GroupRaidGroups)group);
+            session.Character.Group.Update();
+        }
+        
+        [Opcode(ClientMessage.GroupPromoteAssist, "17538")]
+        public static void HandleGroupRaidRole(
+            ref PacketReader packet, WorldClass session)
+        {
+            bool[] guidMask = new bool[8];
+            packet.Skip(1);
+            BitUnpack BitUnpack = new BitUnpack(packet);
+
+            for (int i = 0; i < 4; i++)
+                guidMask[i] = BitUnpack.GetBit();
+
+            byte raidRole = (byte)(BitUnpack.GetBit() ? 1 : 0);
+
+            for (int i = 4; i < 8; i++)
+                guidMask[i] = BitUnpack.GetBit();
+
+
+            ulong GUID = BitUnpack.GetPackedValue(guidMask,
+                new byte[] { 7, 5, 4, 1, 6, 3, 0, 2 },
+                new byte[] { 1, 2, 7, 3, 4, 5, 0, 6 });
+
+            Log.Message(LogType.Error, "{0}", GUID);
+
+            var pSession = WorldMgr.GetSession(GUID);
+
+            if (pSession == null)
+                return;
+
+            pSession.Character.Group.ChangeRaidRole(pSession.Character.Guid, raidRole);
+            session.Character.Group.Update();
+        }
+        
+        // Doesn't work while in raid group
+        [Opcode(ClientMessage.GroupChangeDungeonDifficulty, "17538")]
+        public static void HandleGroupDifficulty(
+            ref PacketReader packet, WorldClass session)
+        {
+            session.Character.Group.DungeonDifficulty = (GroupDungeonDifficulty)packet.ReadUInt32();
+            session.Character.Group.Update();
+        }
+
+        [Opcode(ClientMessage.GroupConvert, "17538")]
+        public static void HandleGroupConvert(
+            ref PacketReader packet, WorldClass session)
+        {
+            var groupType = packet.ReadByte() == 0x00 ? GroupType.Normal : GroupType.Raid;
+
+            session.Character.Group.Type = groupType;
+
+            // atm
+            session.Character.Group.RaidDifficulty = GroupDungeonDifficulty.None;
+            session.Character.Group.DungeonDifficulty = GroupDungeonDifficulty.None;
+
+            session.Character.Group.Update();
+        }
 
         [Opcode(ClientMessage.GroupPromoteLeader, "17538")]
         public static void HandleGroupLeaderChange
@@ -32,7 +140,6 @@ namespace WorldServer.Game.Packets.PacketHandler
                 session.Character.Group.LeaderGUID = newLeader.Character.Guid;
 
             session.Character.Group.Update();
-            //GroupUpdate(session.Character.Group);
         }
 
         [Opcode(ClientMessage.GroupLootUpdate, "17538")]
